0470ded6c041b652fd8f3c394acea18a172278ba
JuChenLin <44205795+JuChenLin@users.noreply.github.com>
2021-03-17 04:22:00 -0500

    Update SkiResortsController.java

--- Ski Resorts in USA/src/main/java/com/priscilla/web/controller/SkiResortsController.java
+++ Ski Resorts in USA/src/main/java/com/priscilla/web/controller/SkiResortsController.java
@@ -2,38 +2,27 @@ package com.priscilla.web.controller;
 
 import com.priscilla.web.entity.enumerate.PriceRange;
 import com.priscilla.web.entity.enumerate.State;
-import com.priscilla.web.entity.service.SkiResortService;
+import com.priscilla.web.service.SkiResortService;
 import com.priscilla.web.entity.skiresort.SkiResort;
 import com.priscilla.web.parameter.SkiResortQueryParameter;
-import com.priscilla.web.repository.SkiResortRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/ski-resorts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SkiResortsController {
 
-//    @Autowired
-//    private SkiResortRepository skiResortRepository;
-
     @Autowired
     private SkiResortService skiResortService;
 
-
-
-    // Get all ski resorts, go to list page
+    // List ski resorts on listing page "/resorts" (template "list.html")
     @GetMapping
     public ResponseEntity<List<SkiResort>> readSkiResorts(@ModelAttribute SkiResortQueryParameter parameter, Model model) {
 //        List<SkiResort> allSkiResorts = skiResortRepository.findAll();
@@ -44,11 +33,10 @@ public class SkiResortsController {
         return ResponseEntity.ok().body(skiResorts);
     }
 
-    // Get the ski resort by ID
+    // Show information of the ski resort of given id on showing page "/resorts/{id}" (template "detail.html")
     @GetMapping("/{id}")
     public ResponseEntity<SkiResort> readSkiResort(@PathVariable("id") Integer id) {
 //        Optional<SkiResort> optionalSkiResort = Optional.ofNullable(skiResortRepository.findById(id).get());
-//
 //        return optionalSkiResort.map( skiResort -> ResponseEntity.ok().body(skiResort) )  //200 OK
 //                .orElseGet( () -> ResponseEntity.notFound().build() );  //404 Not found
 
@@ -56,76 +44,59 @@ public class SkiResortsController {
         return ResponseEntity.ok().body(skiResort);
     }
 
-//    @GetMapping()
-//    public ResponseEntity<List<SkiResort>> searchSkiResorts(@ModelAttribute SkiResortQueryParameter parameter) {
-//        String nameKeyword = parameter.getKeyword();
-//        String orderBy = parameter.getOrderBy();
-//        String sortRule = parameter.getSortRule();
-//
-//        Comparator<SkiResort> comparator = Objects.nonNull(orderBy) && Onjects.nonNull(sortRule)?
-//                                           configureSortComparator(orderBy, sortRule) : (p1, p2) -> 0;
-//
-//        return ResponseEntity.ok().body(searchedSkiResorts);
-//    }
 
+    // Show no information on editing page "/resorts/edit" (template "edit.html")
+    @GetMapping("/edit")
+    public ResponseEntity<Model> toEditPage(Model model) {
+        model.addAttribute("states", State.values());
+        model.addAttribute("priceRanges", PriceRange.values());
 
-////     Go to ski resort editing page
-//    @GetMapping("/resort")
-//    public String toEditPage(Model model) {
-//        model.addAttribute("states", State.values());
-//        model.addAttribute("priceRanges", PriceRange.values());
-//        return "/skiresort/edit";
-//    }
-//
+//        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+//                .path("/edit")
+//                .build()
+//                .toUri();
 
-        // Create new ski resort
-        @PostMapping
-        public ResponseEntity<SkiResort> createSkiResort(@RequestBody SkiResort request) {
-//            skiResortRepository.save(skiResort);
+        return ResponseEntity.ok().body(model);
+    }
 
-            SkiResort skiResort = skiResortService.saveSkiResort(request);
-
-            URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                    .path("/{id}")
-                    .buildAndExpand(skiResort.getId())
-                    .toUri();
-
-            return ResponseEntity.created(location).body(skiResort);
-        }
-
-
-//    @GetMapping("/resort/{id}")
-//    public String toEditPage(@PathVariable("id") String id, Model model) {
-//        SkiResort skiResort = skiResortDao.get(id);
-//
+    // Create a new ski resort
+    // Then show information of this ski resort of given id on showing page "/resorts/{id}" (template "detail.html")
+    @PostMapping
+    public ResponseEntity<SkiResort> createSkiResort(@RequestBody SkiResort request, Model model) {
+        SkiResort skiResort = skiResortService.saveSkiResort(request);
 //        model.addAttribute("resort", skiResort);
-//        model.addAttribute("states", State.values());
-//        model.addAttribute("priceRanges", PriceRange.values());
-//
-//        return "/skiresort/edit";
-//    }
 
-    // Soft delete ski resort
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                                  .path("/{id}")
+                                                  .buildAndExpand(skiResort.getId())
+                                                  .toUri();
+
+        return ResponseEntity.created(location).body(skiResort);
+    }
+
+    // Show information of the ski resort of given id on editing page "/resorts/edit" (template "edit.html")
+    @GetMapping("/edit/{id}")
+    public ResponseEntity<Model> toEditPage(@PathVariable("id") Integer id, Model model) {
+        SkiResort skiResort = skiResortService.getSkiResort(id);
+
+        model.addAttribute("resort", skiResort);
+        model.addAttribute("states", State.values());
+        model.addAttribute("priceRanges", PriceRange.values());
+
+        return ResponseEntity.ok().body(model);
+    }
+
+    // Update the ski resort of given id
+    // Then show information of this ski resort of given id on showing page "/resorts/{id}" (template "detail.html")
     @PutMapping("/{id}")
     public ResponseEntity<SkiResort> updateSkiResort(@PathVariable("id") Integer id, @RequestBody SkiResort request) {
-//        Optional<SkiResort> optionalSkiResort = skiResortRepository.findById(id);
-//
-//        if (!optionalSkiResort.isPresent()) {
-//            return ResponseEntity.notFound().build();  //404 Not found
-//        }
-//
-//        SkiResort skiResort = optionalSkiResort.get();
-//        skiResort.setName(request.getName());
-//        skiResort.setWebsite(request.getWebsite());
-//        skiResort.setPriceRange(request.getPriceRange());
-//        skiResort.setAnnualSnowfall(request.getAnnualSnowfall());
-//        skiResortRepository.save(skiResort);
 
+        System.out.println("Ski Resort Controller -- Request: " + request);
         SkiResort skiResort = skiResortService.updateSkiResort(id, request);
         return ResponseEntity.ok().body(skiResort);  //200 OK
     }
 
-
+    // Soft delete ski resort
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable("id") Integer id){
 //        skiResortRepository.deleteById(id);
