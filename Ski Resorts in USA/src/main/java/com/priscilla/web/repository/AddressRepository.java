b6a8b1b61799f05afeeb6740ce3fc25b78e848ae
JuChenLin <44205795+JuChenLin@users.noreply.github.com>
2021-03-17 04:40:50 -0500

    Update Ski Resort repository
    Add methods:
    - findAllOrderedById(Integer id)
    - findByIsDeletedFalse()
    - findByNameContaining (String name, Sort sort)
    - findByPriceRangeIs(String priceRange, Sort sort)

--- Ski Resorts in USA/src/main/java/com/priscilla/web/repository/AddressRepository.java
+++ Ski Resorts in USA/src/main/java/com/priscilla/web/repository/AddressRepository.java
@@ -4,6 +4,16 @@ import com.priscilla.web.entity.key.AddressID;
 import com.priscilla.web.entity.skiresort.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressRepository extends JpaRepository<Address, AddressID> {
+import java.io.Serializable;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
 
 }
+
+//public interface AddressRepository extends JpaRepository<Address, AddressID> {
+//
+//}
+
+//public interface AddressRepository<Address, AddressID extends Serializable> extends JpaRepository<Address, AddressID> {
+//
+//}
--- Ski Resorts in USA/src/main/java/com/priscilla/web/repository/SkiResortRepository.java
+++ Ski Resorts in USA/src/main/java/com/priscilla/web/repository/SkiResortRepository.java
@@ -1,8 +1,24 @@
 package com.priscilla.web.repository;
 
 import com.priscilla.web.entity.skiresort.SkiResort;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
 public interface SkiResortRepository extends JpaRepository<SkiResort, Integer> {
+
+    // Select All
+    List<SkiResort> findAllOrderedById(Integer id);
+
+    // Select not Deleted
+    List<SkiResort> findByIsDeletedFalse();
+
+    // Name Query
+    List<SkiResort> findByNameContaining (String name, Sort sort);
+
+    // priceRange Queries
+    List<SkiResort> findByPriceRangeIs(String priceRange, Sort sort);
 
 }
