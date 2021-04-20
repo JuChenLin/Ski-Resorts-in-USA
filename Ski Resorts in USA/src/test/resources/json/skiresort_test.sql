b6e21833503d0d48e8a2476b993d04170941af49
JuChenLin <44205795+JuChenLin@users.noreply.github.com>
2021-04-20 15:05:45 -0500

    Create skiresort_test.sql

---
+++ Ski Resorts in USA/src/test/resources/json/skiresort_test.sql
@@ -0,0 +1,43 @@
+[
+    {
+        "name": "HY",
+        "website": "www.hy.com",
+        "priceRange": "HIGH",
+        "annualSnowfall": 350,
+        "mountainStat": {
+            "acres": 3800,
+            "peakElevation": 9067,
+            "baseElevation": 6170,
+            "numRuns": 98,
+            "numLifts": 29,
+            "numTerrainParks": 4,
+            "pctBeginnerTerrain": 8,
+            "pctIntermediateTerrain": 62,
+            "pctAdvancedTerrain": 25,
+            "pctExpertTerrain": 5
+        },
+        "addresses": [
+            {
+                "lodge": "CA Lodge",
+                "street": "3860 Saddle Road",
+                "city": "South Lake Tahoe",
+                "state": "CA",
+                "zipCode": "96150"
+            },
+            {
+                "lodge": "NV Lodge",
+                "street": "375 Quaking Aspen Lane",
+                "city": "Stateline",
+                "state": "NV",
+                "zipCode": "89449"
+            },
+            {
+                "lodge": "Boulder Lodge",
+                "street": "140 S. Benjamin Drive",
+                "city": "Stateline",
+                "state": "NV",
+                "zipCode": "89449"
+            }
+        ]
+    }
+]
